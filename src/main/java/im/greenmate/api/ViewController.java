005eaae5671cb5265e4d18956986afae072067e9
seungtaekhong <hsk4991149@naver.com>
2023-09-08 14:53:27 +0900

    initial project

---
+++ src/main/java/im/greenmate/api/ViewController.java
@@ -0,0 +1,13 @@
+package im.greenmate.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ViewController {
+
+    @GetMapping
+    public String indexPage() {
+        return "Hello, world!";
+    }
+}
---
+++ src/test/java/im/greenmate/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package im.greenmate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
