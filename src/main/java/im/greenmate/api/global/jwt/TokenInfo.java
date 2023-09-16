7fcd300bf4546e0b4f17bfe85d8f7afbe6fb2406
seungtaekhong <hsk4991149@naver.com>
2023-09-16 19:46:58 +0900

    feat: create JwtTokenProvider

---
+++ src/main/java/im/greenmate/api/global/jwt/TokenInfo.java
@@ -0,0 +1,14 @@
+package im.greenmate.api.global.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenInfo {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
